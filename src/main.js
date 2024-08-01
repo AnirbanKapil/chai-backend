5ee203c67c7c66c7799b56cee447c976df80299e
Anirban Kapil <anirbankapil@gmail.com>
2024-08-02 00:53:34 +0530

    connected to mongoDB

--- src/main.js
+++ src/main.js
@@ -3,49 +3,11 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })  
 
-// import mongoose from "mongoose";
-
-// import {DB_NAME} from "./constants";
 
 connectDB();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import express from "express";
-
-// const app = express();
-
-// (async()=>{
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB.URI}/${DB_NAME}`)
-//         app.on("error",(error)=>{
-//             console.log("ERROR :", error);
-//             throw error
-//         })
-//         app.listen(process.env.PORT,()=>{
-//             console.log(`app listening on Port ${process.env.PORT}`)
-//         })
-//     } catch (error) {
-//         console.log("ERROR :", error)
-//         throw error
-//     }
-// })()
