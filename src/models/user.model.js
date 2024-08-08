4b76504aa3354694451162c2329c87e1c288cfb6
Anirban Kapil <anirbankapil@gmail.com>
2024-08-08 23:57:22 +0530

    jwt setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -65,5 +65,13 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password,this.password)
 }
 
+userSchema.methods.generateAccessToken = function() {
+
+}
+
+
+userSchema.methods.generateRefreshToken = function() {
+    
+}
 
 export const User = mongoose.model("User",userSchema)
