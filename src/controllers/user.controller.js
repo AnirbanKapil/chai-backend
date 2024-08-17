b4f5a675f97c694a4b81d98821be8eb1f213a678
Anirban Kapil <anirbankapil@gmail.com>
2024-08-17 17:27:12 +0530

    getting user's detail

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,8 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message : "ok"
-    })
+    const {username,email,fullname,password} = req.body
+    
 })
 
 export {registerUser};
