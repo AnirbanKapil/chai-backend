54612a3259ded460fb8eb46f57e7b88dc09900f5
Anirban Kapil <anirbankapil@gmail.com>
2024-08-18 00:07:19 +0530

    validate user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,15 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
-
+import {ApiError} from "../utils/ApiError.js"
 
 const registerUser = asyncHandler(async (req,res) => {
     const {username,email,fullname,password} = req.body
+    
+    if(
+        [username,email,fullname,password].some((field)=>
+        field?.trim() === "")
+        ){
+            throw new ApiError(400,"All the fields are required")
+        } 
     
 })
 
