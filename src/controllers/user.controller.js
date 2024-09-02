9fb75bf0b9f6d015fdf1798dc4fc263840c3b4fe
Anirban Kapil <anirbankapil@gmail.com>
2024-09-02 20:19:04 +0530

    update account details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -234,10 +234,32 @@ const getCurrentUser = asyncHandler(async (req,res) => {
 })
 
 
+const updateAccountDetail = asyncHandler(async (req,res) => {
+    
+    const {fullName,email} = req.body
+
+    if(!(fullName || email)){
+        throw new ApiError(401,"All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(req.user?._id,{
+        $set : {
+            fullName : fullName,
+            email : email
+        }
+    },{new : true}
+    ).select("-password")
+
+    return res.status(200)
+              .json(new ApiResponse(200,user,"Account details updated successfully"))
+})
+
+
 export {registerUser,
         loginUser, 
         logoutUser,
         refreshAccessToken,
         changeCurrentPassword,
-        getCurrentUser  
+        getCurrentUser,
+        updateAccountDetail  
 };
