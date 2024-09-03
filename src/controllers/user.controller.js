c0238c9bdc05fcf188ff2ee84be6ca4db22ea281
Anirban Kapil <anirbankapil@gmail.com>
2024-09-03 22:03:43 +0530

    updat user cover image

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -269,7 +269,7 @@ const updateUserAvatar = asyncHandler(async (req,res) => {
         throw new ApiError(400,"Error while uploading avatar")
     }
 
-    await User.findByIdAndUpdate(req.user?._id,
+    const user = await User.findByIdAndUpdate(req.user?._id,
         {
           $set : {
             avatar : avatar.url
@@ -277,6 +277,37 @@ const updateUserAvatar = asyncHandler(async (req,res) => {
         },
         {new : true}
     ).select("-password")
+
+    return res.status(200)
+              .json(new ApiResponse(200,user,"Avatar image updated successfully")) 
+})
+
+
+const updateUserCoverImage = asyncHandler(async (req,res) => {
+
+    const coverImageLocalPath = req.file?.path
+
+    if(! coverImageLocalPath){
+        throw new ApiError(400,"Cover Image file missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(! coverImage.url){
+        throw new ApiError(400,"Error while uploading cover image")
+    }
+
+    const user = await User.findByIdAndUpdate(req.user?._id,
+        {
+         $set : {
+            coverImage : coverImage.url
+         }
+    },
+    {new : true}
+    ).select("-password")
+
+    return res.status(200)
+    .json(new ApiResponse(200,user,"Cover image updated successfully"))
 })
 
 
@@ -287,5 +318,6 @@ export {registerUser,
         changeCurrentPassword,
         getCurrentUser,
         updateAccountDetail,
-        updateUserAvatar  
+        updateUserAvatar,
+        updateUserCoverImage  
 };
