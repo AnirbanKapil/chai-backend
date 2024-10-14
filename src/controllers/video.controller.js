d8113c07d904278804dd3398c9d7bc7289a0dfa4
Anirban Kapil <anirbankapil@gmail.com>
2024-10-15 01:03:45 +0530

    upload video controller

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {User} from "../models/user.model.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { Video } from "../models/video.model.js";
+
+
+const publishAVideo = asyncHandler(async(req,res) => {
+    const {title,description} = req.body
+    
+    try {
+        if(
+            [title,description].some((field) => field.trim() === "")
+          )
+            {
+                throw new ApiError(400,"title & description fields required")
+            }
+        
+        
+        const videoLocalPath = req.files?.videoFile[0]?.path
+        const thumbnailLocalPath = req.files?.thumbnail[0]?.path
+    
+        if(!videoLocalPath){
+            throw new ApiError(400,"video file not found")
+            }   
+        if(!thumbnailLocalPath){
+            throw new ApiError(400,"video file not found")
+            }      
+        
+        const uploadedVideo = await uploadOnCloudinary(videoLocalPath)
+        const uploadedThumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+    
+        if(!uploadedVideo){
+            throw new ApiError(500,"error while uploading on cloudinary")
+        }
+        if(!uploadedThumbnail){
+            throw new ApiError(500,"error while uploading on cloudinary")
+        }
+    
+        const videoInfo = await Video.create({
+             title : title,
+             description : description,
+             videoFile : uploadedVideo.url,
+             thumbnail : uploadedThumbnail.url,
+             duration : uploadedVideo.duration,
+             owner : req.user?._id
+        })
+    
+        if(!videoInfo){
+            throw new ApiError(500,"error while uploading")
+        }
+    
+        return res.status(200)
+                  .json(new ApiResponse(200,{videoInfo},"upload successfull"))
+    } catch (error) {
+        console.log(`error msg : ${error.message} ||`)
+    }
+})
+
+
+export {publishAVideo}
