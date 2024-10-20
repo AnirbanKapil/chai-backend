d54a27c3ea926de604345e1fe4c84985c4226f9a
Anirban Kapil <anirbankapil@gmail.com>
2024-10-20 23:57:35 +0530

    tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -2,6 +2,8 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Tweet } from "../models/tweet.model.js";
+import { isValidObjectId } from "mongoose";
+import {User} from "../models/user.model.js"
 
 
 const createTweet = asyncHandler(async (req,res) => {
@@ -21,6 +23,30 @@ const createTweet = asyncHandler(async (req,res) => {
 }) 
 
 
+const getUserTweets = asyncHandler(async (req, res) => {
+    const {userId} = req.params
+    
+    if(!userId){
+        throw new ApiError(400,"Id not found")
+    }
+    
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400,"Id not valid")
+    }
+
+
+    const tweets = await Tweet.find({
+        owner : userId
+    }).select("-owner")
+
+    if(!tweets?.length){
+        throw new ApiError(404,"User doesnot exist")
+    }
+
+    return res.status(200)
+              .json(new ApiResponse(200,{tweets},"Fetched tweets successfully"))
+
+})
 
 
 
@@ -29,4 +55,5 @@ const createTweet = asyncHandler(async (req,res) => {
 
 
 
-export {createTweet}
+
+export {createTweet,getUserTweets}
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet , getUserTweets } from "../controllers/tweet.controller.js";
+
 
 const router = Router()
 
 router.route("/").post(verifyJWT,createTweet)
+router.route("/user/:userId").get(getUserTweets)
 
 
 
